dee37ed4fd0540195e23da169530ed84ee5d806d
Hauke Schulz <hauke27@gmail.com>
2018-04-25 22:43:46 +0200

    Added comments and fixed a return type

--- src/Tree/TreeNodeInterface.php
+++ src/Tree/TreeNodeInterface.php
@@ -7,43 +7,49 @@ namespace hschulz\DataStructures\Tree;
  */
 interface TreeNodeInterface
 {
-
     /**
+     * Adds a TreeNode to the tree.
      *
-     * @param TreeNodeInterface $node
+     * @param TreeNodeInterface $node The new node
      * @return void
      */
-    public function addChild(self $node);
+    public function addChild(self $node): void;
 
     /**
+     * Returns the array representation of the tree.
      *
      * @return array
      */
     public function getChildren(): array;
 
     /**
+     * Returns the tree node at the given position.
      *
-     * @param int $index
-     * @return TreeNodeInterface
+     * @param int $index The index position
+     * @return TreeNodeInterface|null The node at the given position
      */
-    public function getChildAt(int $index): self;
+    public function getChildAt(int $index): ?self;
 
     /**
+     * Returns the number of all nodes in this tree.
      *
-     * @return int
+     * @return int The number of all nodes.
      */
     public function getChildCount(): int;
 
     /**
+     * Returns the index position of the given node in this tree.
+     * Will return -1 if the node was not found in this tree.
      *
      * @param TreeNodeInterface $node
-     * @return int
+     * @return int The index position or -1 if the node was not found.
      */
     public function getIndex(self $node): int;
 
     /**
+     * Returns true if the tree has no child elements.
      *
-     * @return bool
+     * @return bool True if there are no child elements in this tree.
      */
     public function isLeaf(): bool;
 }
