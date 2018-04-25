1951c18158cafe196ca7e91d5b916cad4b33d9aa
Hauke Schulz <hauke27@gmail.com>
2018-04-25 22:45:07 +0200

    Small updates for code style and comments

--- src/Tree/AbstractTreeNode.php
+++ src/Tree/AbstractTreeNode.php
@@ -2,38 +2,39 @@
 
 namespace hschulz\DataStructures\Tree;
 
-use \array_search;
-use \count;
+use function \array_search;
+use function \count;
 
 /**
  *
  */
 abstract class AbstractTreeNode implements TreeNodeInterface
 {
-
     /**
-     *
+     * The actual node data.
      * @var array
      */
     protected $children = [];
 
     /**
-     *
+     * Will be false when any child nodes are added.
      * @var bool
      */
     protected $isLeaf = true;
 
     /**
-     *
+     * Creates a new leaf node.
      */
     public function __construct()
     {
         $this->children = [];
+        $this->isLeaf = true;
     }
 
     /**
+     * Adds the given tree node to this nodes child list.
      *
-     * @param TreeNodeInterface $node
+     * @param TreeNodeInterface $node The node to be inserted
      * @return void
      */
     public function addChild(TreeNodeInterface $node): void
@@ -45,33 +46,33 @@ abstract class AbstractTreeNode implements TreeNodeInterface
     /**
      *
      * @param int $index
-     * @return TreeNodeInterface
+     * @return TreeNodeInterface|null
      */
-    public function getChildAt(int $index): TreeNodeInterface
+    public function getChildAt(int $index): ?TreeNodeInterface
     {
         return empty($this->children[$index]) ? null : $this->children[$index];
     }
 
     /**
+     * Returns the total sum of all children and their children.
      *
-     * @return int
+     * @return int The total sum of all children
      */
     public function getSumChildCount(): int
     {
         $count = count($this->children);
 
-        if ($count > 0) {
-            foreach ($this->children as $node) {
-                $count += $node->getChildCount();
-            }
+        foreach ($this->children as $node) {
+            $count += $node->getChildCount();
         }
 
         return $count;
     }
 
     /**
+     * Returns the number of child nodes for this node.
      *
-     * @return int
+     * @return int The number of child nodes.
      */
     public function getChildCount(): int
     {
@@ -79,8 +80,9 @@ abstract class AbstractTreeNode implements TreeNodeInterface
     }
 
     /**
+     * Returns an array containing the child nodes of this node.
      *
-     * @return array
+     * @return array The child nodes or an empty array
      */
     public function getChildren(): array
     {
@@ -95,12 +97,13 @@ abstract class AbstractTreeNode implements TreeNodeInterface
     public function getIndex(TreeNodeInterface $node): int
     {
         $index = array_search($node, $this->children, true);
-        return ($index === false) ? -1 : $index;
+        return $index === false ? -1 : $index;
     }
 
     /**
+     * Returns true if this node doesn't have child nodes.
      *
-     * @return bool
+     * @return bool True if this node doesn't have no child nodes.
      */
     public function isLeaf(): bool
     {
