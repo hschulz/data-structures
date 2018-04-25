df81d60e30296651b95fa34d3a61f621c7aa0fed
Hauke Schulz <hauke27@gmail.com>
2018-04-25 22:41:27 +0200

    Completely reworked the PriorityQueue
    The class now directly extends the SplPriorityQueue and only implements
    the Serializable interface.

--- src/Queue/PriorityQueue.php
+++ src/Queue/PriorityQueue.php
@@ -2,120 +2,93 @@
 
 namespace hschulz\DataStructures\Queue;
 
-use \Countable;
-use \IteratorAggregate;
 use \Serializable;
-use \serialize;
 use \SplPriorityQueue;
-use \unserialize;
+use function \serialize;
+use function \unserialize;
 
 /**
  *
  */
-class PriorityQueue implements Countable, IteratorAggregate, Serializable
+class PriorityQueue extends SplPriorityQueue implements Serializable
 {
     /**
-     *
+     * The default priority value used when no priority is given.
      * @var int
      */
     const PIRORITY_DEFAULT = 1;
 
     /**
+     * Iterates the queue and returns an array containing each item with
+     * the data and priority set.
      *
-     * @var SplPriorityQueue
-     */
-    protected $queue = null;
-
-    /**
-     *
-     * @var array
-     */
-    protected $items = [];
-
-    /**
-     *
-     */
-    public function __construct()
-    {
-        $this->items = [];
-        $this->queue = new SplPriorityQueue();
-    }
-
-    /**
-     *
-     * @return array
+     * @return array The array representation of the queue
      */
     public function toArray(): array
     {
-        return $this->items;
+        /* Enable extraction of data and priority */
+        $this->setExtractFlags(self::EXTR_BOTH);
+
+        /* Prepare output */
+        $data = [];
+
+        /* Iterate yourself */
+        foreach ($this as $item) {
+            $data[] = $item;
+        }
+
+        return $data;
     }
 
     /**
+     * Allows inserting data into the queue without explicitly setting a
+     * priority value. Inserts without a priority will use the PRIORITY_DEFAULT
+     * value.
      *
-     * @param type $data
-     * @param int $priority
+     * @param mixed $data The data to insert
+     * @param mixed $priority The priority
      * @return void
      */
-    public function insert($data, int $priority = self::PIRORITY_DEFAULT): void
+    public function insert($data, $priority = self::PIRORITY_DEFAULT): void
     {
-        $this->items[] = [
-            'data' => $data,
-            'priority' => $priority
-        ];
-
-        $this->queue->insert($data, $priority);
+        parent::insert($data, $priority);
     }
 
     /**
+     * Serializes the queue by converting the queue into an array.
      *
-     * @return int
-     */
-    public function count(): int
-    {
-        return $this->queue->count();
-    }
-
-    /**
-     *
-     * @return SplPriorityQueue
-     */
-    public function getIterator(): SplPriorityQueue
-    {
-        return clone $this->queue;
-    }
-
-    /**
-     *
-     * @return string
+     * @return string The serialized queue
      */
     public function serialize()
     {
-        return serialize($this->items);
+        return serialize($this->toArray());
     }
 
     /**
+     * Unserializes the queue.
      *
-     * @param string $queueData
+     * @param string $queue The serialized queue data
      * @return void
      */
-    public function unserialize($queueData)
+    public function unserialize($queue)
     {
-        foreach (unserialize($queueData) as $element) {
-            $this->insert($element['data'], $element['priority']);
+        foreach (unserialize($queue) as $item) {
+            $this->insert($item['data'], $item['priority']);
         }
     }
 
     /**
+     * Merges the given queue data into this queue.
      *
-     * @param PriorityQueue $queue
+     * @param PriorityQueue $queue The queue to merge
      * @return void
      */
     public function merge(self $queue): void
     {
         $data = $queue->toArray();
 
-        foreach ($data as $element) {
-            $this->insert($element['data'], $element['priority']);
+        foreach ($data as $item) {
+            $this->insert($item['data'], $item['priority']);
         }
     }
 }
